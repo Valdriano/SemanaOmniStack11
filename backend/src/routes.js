b7a153a44f45931690a3fc275389e7f9bf038c88
Valdriano Oliveira <valdriano.oliveira@gmail.com>
2020-03-26 18:29:07 -0400

    backend

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentsController');
+const ProfileConntroller = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index );
+routes.post( '/ongs', OngController.create );
+routes.get('/incidents',IncidentsController.index);
+routes.post('/incidents',IncidentsController.create);
+routes.delete('/incidents/:id',IncidentsController.delete);
+routes.get('/profile',ProfileConntroller.index);
+routes.post('/sessions',SessionController.create);
+
+module.exports = routes;
